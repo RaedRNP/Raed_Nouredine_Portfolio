85c152f8f73135160b0783a2d3416e1088b74f6c
Raed <raedrnp10@gmail.com>
2024-03-30 01:57:20 -0400

    change domain name

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  site: "https://raedrnp10.github.io",
+  site: "https://raedrnp.github.io",
 });
